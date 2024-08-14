fd1b83e9058b81ee21b71432cb366e03fae4f710
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-08-14 12:36:04 +0000

    Рефакторинг

--- src/app/main.py
+++ src/app/main.py
@@ -4,8 +4,11 @@ from src.app.routers.transaction import router
 
 app = FastAPI()
 
-@app.get("/")
-def read_main():
-    return {"message": "Welcome to the Transaction Service API"}
 
-app.include_router(router, prefix="/transaction", tags=["transaction"])
+@app.get('/')
+def read_main():
+    """Функция с приветственным текстом."""
+    return {'message': 'Welcome to the Transaction Service API'}
+
+
+app.include_router(router, prefix='/transaction', tags=['transaction'])
