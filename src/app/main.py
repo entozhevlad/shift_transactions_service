a9e3590f8d7adc2c3984e4b8670baa1049529e14
Vladislav Zarubin <vladislavzarubin113@gmail.com>
2024-09-09 23:18:49 +0700

    Добавлен jaeger

--- src/app/main.py
+++ src/app/main.py
@@ -1,9 +1,24 @@
 from fastapi import FastAPI
-
+from opentelemetry import trace
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.exporter.jaeger.proto.grpc import JaegerExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from src.app.routers.transaction import router
 
 app = FastAPI()
 
+trace.set_tracer_provider(TracerProvider())
+jaeger_exporter = JaegerExporter(
+    agent_host_name='jaeger',
+    agent_port=6831,
+)
+span_processor = BatchSpanProcessor(jaeger_exporter)
+trace.get_tracer_provider().add_span_processor(span_processor)
+
+FastAPIInstrumentor.instrument_app(app)
+RequestsInstrumentor().instrument()
 
 @app.get('/')
 def read_main():
